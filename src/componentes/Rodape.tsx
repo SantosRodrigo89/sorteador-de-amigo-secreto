3140b3c20376ace9b00cd51a747714d489f18c0c
rodrigovieiraminu <rodrigo.vieira@minu.co>
2024-09-23 15:01:33 -0300

    feat: componente rodape com rota

--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -5,11 +5,17 @@ import { useNavigate } from "react-router-dom";
 const Rodape = () => {
   const participantes = useListaDeParticipantes();
 
-  // const navegarPara = useNavigate
+  const navegarPara = useNavigate();
+
+  const iniciar = () => {
+    navegarPara("/sorteio");
+  };
 
   return (
     <footer>
-      <button disabled={participantes.length < 3}>Iniciar sorteio</button>
+      <button disabled={participantes.length < 3} onClick={iniciar}>
+        Iniciar sorteio
+      </button>
     </footer>
   );
 };
