5419d78439f1d8ed9b34db8aea3b09d691536986
rodrigovieiraminu <rodrigo.vieira@minu.co>
2024-09-23 15:25:58 -0300

    feat: nova pagina de configuracao

---
+++ src/componentes/paginas/Configuracao.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formulario from '../Formulario'
+import ListaDeParticipantes from '../ListaDeParticipantes'
+import Rodape from '../Rodape'
+
+const Configuracao = () => {
+  return (
+    <div>
+      <Formulario />
+      <ListaDeParticipantes />
+      <Rodape />
+    </div>
+  )
+}
+
+export default Configuracao
